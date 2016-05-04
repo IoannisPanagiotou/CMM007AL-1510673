ade4ac19a2a6c663b947b25c06b6b9dbc6ea893a
1510673 <1510673@rgu.ac.uk>
2016-05-04 11:41:52 +0100

    0

--- blog.php
+++ blog.php
@@ -48,7 +48,7 @@ include("dbconnect.php");
                     echo "{$entryTitle} by {$submitter} <br> {$category} <br> {$entrySummary}<br><hr />";
                 }
 
-                $category=$POST_["category"];
+                $category=$GET_["category"];
                 $sql2="SELECT * FROM blogview WHERE category='$category'";
                 $result2=mysqli_query($db,$sql2);
                 if (mysqli_num_rows($result2)==1)
